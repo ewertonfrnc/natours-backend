8bd6beafd0a95a0ee6f93abaafcbe5ff52c4d08a
Ewerton <ewerton.silva@usemobile.xyz>
2023-10-03 23:29:37 -0300

    feat: changed file structure, added .env variables and configured eslint with prettier

--- app.js
+++ app.js
@@ -1,80 +1,25 @@
+const morgan = require('morgan');
 const express = require('express');
-const fs = require('fs');
+
+const tourRouter = require('./routes/tour.routes');
+const userRouter = require('./routes/user.routes');
 
 const app = express();
+
+// MIDDLEWARE
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use(express.json());
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (request, response) => {
-  response.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: { tours },
-  });
-};
-
-const getTour = (request, response) => {
-  const tour = tours.find((tour) => tour.id === +request.params.id);
-  if (!tour)
-    return response.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID.',
-    });
-
-  response.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
-};
-
-const createTour = (request, response) => {
-  const newID = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newID }, request.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      response.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
-};
-
-const upddateTour = (request, response) => {
-  if (request.params.id > tours.length)
-    return response.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID.',
-    });
-
-  response.status(200).json({
-    status: 'success',
-    data: { tour: '<updated tour here>...' },
-  });
-};
-
-const deleteTour = (request, response) => {
-  response.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(upddateTour)
-  .delete(deleteTour);
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
+app.use((request, response, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
+app.use((request, response, next) => {
+  request.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
