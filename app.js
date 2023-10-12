07bddad41a720e774d0cf90e9944ee323de72bf5
Ewerton <ewefsilva@gmail.com>
2023-10-11 23:55:35 -0300

    feat: Implemented a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -19,9 +19,25 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (request, response, next) => {
-  response.status(404).json({
-    status: 'fail',
-    message: `Can't find ${request.originalUrl} on this server!`,
+  const error = new Error(`Can't find ${request.originalUrl} on this server!`);
+  error.status = 'fail';
+  error.statusCode = 404;
+
+  next(error);
+
+  // response.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${request.originalUrl} on this server!`,
+  // });
+});
+
+app.use((error, request, response, next) => {
+  error.statusCode = error.statusCode || 500;
+  error.status = error.status || 'error';
+
+  response.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
   });
 });
 
